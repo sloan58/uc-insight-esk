016ebfa279bf685b5ec54f6668b6bbef6fa145bb
Martin Sloan <martinsloan@Martins-iMac.local>
2016-02-08 14:13:55 -0500

    remove and readd email template

--- resources/views/emails/SecondaryDialToneReport.blade.php
+++ resources/views/emails/SecondaryDialToneReport.blade.php
@@ -4,8 +4,8 @@
 
 @include('emails.templates.widgets.articleStart')
 
-    <h4 class="secondary"><strong>CUCM None Partition Report</strong></h4>
-    <p>Please see the attached report of DN's found in the CUCM 'None' Partition</p>
+<h4 class="secondary"><strong>CUCM None Partition Report</strong></h4>
+<p>Please see the attached report of DN's found in the CUCM 'None' Partition</p>
 
 @include('emails.templates.widgets.articleEnd')
 
