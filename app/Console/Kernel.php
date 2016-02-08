3e277cd98308bb69ba91a72cf428eb1c5e649411
Martin Sloan <martinsloan@Martins-iMac.local>
2016-02-08 17:09:45 -0500

    add Aaron to reporting and send once daily at 8am

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('cucm:none-pt')->everyFiveMinutes();
-//                 ->dailyAt('08:00');
+        $schedule->command('cucm:none-pt')
+                 ->dailyAt('08:00');
     }
 }
