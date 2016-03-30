8be7440fe658e6c42506792578195f6701d4955c
sloan58 <martin.sloan@karma-tek.com>
2016-03-30 00:42:14 -0400

    change message on new groups panel

--- resources/themes/default/views/duo/show.blade.php
+++ resources/themes/default/views/duo/show.blade.php
@@ -170,7 +170,7 @@
             <div class="col-md-10">
                 <div class="box box-warning">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Assigned Groups <h6>(Duo Assigned Groups Cannot Be Removed)</h6></h3>
+                        <h3 class="box-title">Assigned Groups <h6>Note: Groups assigned within Duo itself cannot be removed here.</h6></h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
