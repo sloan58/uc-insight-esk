1782e696af9ae5cbd00d062ff8ea7ebd685a01b4
= <=>
2016-04-05 15:03:15 -0400

    strip original vars for final file

--- app/Http/Controllers/IosConfigGeneratorController.php
+++ app/Http/Controllers/IosConfigGeneratorController.php
@@ -38,29 +38,42 @@ class IosConfigGeneratorController extends Controller
      */
     public function create($fileName)
     {
-
+        //Get the file from storage
         $contents = Storage::get('ios-config-templates/' .  $fileName);
 
+        //Create an empty array to fill with config headers and vars
         $viewVariables = [];
 
+        //Find all the headers (begin and end)
         preg_match_all('/{.+?}/',$contents,$begTags);
 
+        //Loop each header
         foreach($begTags[0] as $index => $tag)
         {
+            //The headers are stored with the 'start' marker in
+            //even indices.  If it's not even, it's an 'end' marker
+            //and we can continue;
             if($index % 2 != 0) continue;
 
+            //Rip out { } from the headers to display nicely in HTML
             $viewHeader = trim(str_replace(['{','}'],'',$tag));
 
+            //Get the beginning position of the section
             $beginPos = strpos($contents,$tag);
+            //Get the ending position of the section
             $endPos = strpos($contents,$begTags[0][$index + 1]);
+            //Get the length between beginning and end
             $length = abs($beginPos - $endPos);
 
+            //Extract the text between the header markers
             $between = substr($contents, $beginPos, $length);
 
+            //Get an array of variable within the section
             preg_match_all('/<<.+?>>/',$between,$matches);
-
             $matches = $matches[0];
 
+            //Loop the variables and create friendly display
+            //names for the HTML
             foreach($matches as $match)
             {
                 preg_match('/<<(.*)>>/',$match,$out);
@@ -70,7 +83,6 @@ class IosConfigGeneratorController extends Controller
 
         }
 
-//        dd($viewVariables);
         return view('ios-config-generator.create',compact('viewVariables','fileName'));
     }
 
@@ -80,16 +92,30 @@ class IosConfigGeneratorController extends Controller
      */
     public function store(Request $request)
     {
+        //Get the form input
         $input = $request->input();
+
+        //Get the file from storage
         $contents = Storage::get('ios-config-templates/' .  $input['fileName']);
 
+        //Find all the variables in the file
         preg_match_all('/<<.+?>>/',$contents,$matches);
-
         $matches = $matches[0];
 
+        //Loop each variable and replace with the
+        //actual data submitted in the form
         foreach($matches as $match)
         {
             $contents = str_replace($match,$input[$match],$contents);
+        }
+
+        //Find all the headers (begin and end)
+        preg_match_all('/{.+?}/',$contents,$begTags);
+
+        //Loop each header to remove it from the final file
+        foreach($begTags[0] as $index => $tag)
+        {
+            $contents = str_replace($tag,'',$contents);
         }
 
         $tempFileName = 'ios-config-templates/temp/'. $input['fileName'] . '-' . 'completed' . '-' . \Carbon\Carbon::now()->timestamp . '.txt';
