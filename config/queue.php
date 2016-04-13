610bea68ac0e76aa50c260b9a420584958081b60
sloan58 <martin.sloan@karma-tek.com>
2016-04-13 14:35:40 -0400

    add sqs queues

--- config/queue.php
+++ config/queue.php
@@ -51,9 +51,9 @@ return [
 
         'sqs' => [
             'driver' => 'sqs',
-            'key'    => 'your-public-key',
-            'secret' => 'your-secret-key',
-            'queue'  => 'your-queue-url',
+            'key'    =>  env('SQS_KEY'),
+            'secret' =>  env('SQS_SECRET'),
+            'queue'  =>  env('SQS_QUEUE'),
             'region' => 'us-east-1',
         ],
 
